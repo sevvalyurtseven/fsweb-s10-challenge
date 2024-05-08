46bd30b042d1a7968e4cd965ee628fa744afbc11
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-08 08:06:52 +0300

    action type constants ve action creatorlar olusturuldu

--- src/actions.js
+++ src/actions.js
@@ -6,22 +6,22 @@ export const FETCH_LOADING = "YUKLENIYOR";
 export const FETCH_ERROR = "HATA VAR";
 
 export function notEkle(not) {
-  return {type: NOT_EKLE, payload: not};
+  return { type: NOT_EKLE, payload: not };
 }
 
 export function notSil(notId) {
-  return {type: NOT_SIL, payload: notId};
+  return { type: NOT_SIL, payload: notId };
 }
 
 export function fetchLoading() {
-  return {type: FETCH_LOADING};
+  return { type: FETCH_LOADING };
 }
 
-export function fetchError(message){
-  return {type: FETCH_ERROR, payload: message};
+export function fetchError(message) {
+  return { type: FETCH_ERROR, payload: message };
 }
 
-export const notEkleAPI = (yeniNot) => dispatch => {
+export const notEkleAPI = (yeniNot) => (dispatch) => {
   dispatch(fetchLoading());
   axios
     .post("https://httpbin.org/anything", yeniNot)
@@ -32,24 +32,24 @@ export const notEkleAPI = (yeniNot) => dispatch => {
       }
     })
     .catch((error) => {
-      console.log(error)
-    dispatch(fetchError(error.res.message))
-  });
-}
+      console.log(error);
+      dispatch(fetchError(error.response.message));
+    });
+};
 
-export const notSilAPI = (id) => dispatch => {
-  dispatch(fetchLoading())
-  console.log(id)
+export const notSilAPI = (id) => (dispatch) => {
+  dispatch(fetchLoading());
+  console.log(id);
   axios
     .delete("https://httpbin.org/anything", { data: id })
     .then((res) => {
       if (res.status === 200) {
-        dispatch(notSil(res.data.json))
-        // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notSil ile dispatch edin 
+        dispatch(notSil(res.data.data));
+        // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notSil ile dispatch edin
       }
     })
     .catch((error) => {
-      console.log(error)
-    dispatch(fetchError(error.res.message))
-  });
-}
+      console.log(error);
+      dispatch(fetchError(error.response.message));
+    });
+};
