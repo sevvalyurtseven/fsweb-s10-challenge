7deae4bfc26080a0a31a276bbd3078fee509c0a9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-08 08:07:45 +0300

    store'a logger eklendi

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import thunk from "redux-thunk";
 import reducer from "../reducers";
+import logger from "redux-logger";
 
-export const myStore = createStore(reducer, applyMiddleware(thunk));
+
+export const myStore = createStore(reducer, applyMiddleware(thunk,logger));
